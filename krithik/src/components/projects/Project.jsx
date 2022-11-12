9dc40dae9aeb28db4f5382d16aca502621471626
Krithik <krithiksehgal@gmail.com>
2022-11-12 06:23:39 -0800

    project file changes

--- krithik/src/components/projects/Project.jsx
+++ krithik/src/components/projects/Project.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Box from '@mui/material/Box'
 import './projects.css'
-import { Grid } from '@mui/material'
 
 const Project = (props) => {
   return (
@@ -13,17 +12,9 @@ const Project = (props) => {
             textAlign: 'center',
             borderRadius: '10px'
         }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <h3><a href={props.link}>{props.title}</a></h3>
-                </Grid>
-                <Grid item xs={12}>
-                    <p>{props.description}</p>
-                </Grid>
-                <Grid item xs={12}>
-                    <p>{props.tech}</p>
-                </Grid>
-            </Grid>
+            <h3><a href={props.link}>{props.title}</a></h3>
+            <p>{props.description}</p>
+            <p>{props.tech}</p>
         </Box>
     </div>
   )
